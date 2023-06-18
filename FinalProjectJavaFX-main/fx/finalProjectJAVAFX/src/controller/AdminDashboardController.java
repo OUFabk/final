17a4930c2fb01f6f80da4c5d20c06d3c381f7401
OUFabk <135167055+OUFabk@users.noreply.github.com>
2023-06-18 05:07:52 +0300

    Update AdminDashboardController.java

--- FinalProjectJavaFX-main/fx/finalProjectJAVAFX/src/controller/AdminDashboardController.java
+++ FinalProjectJavaFX-main/fx/finalProjectJAVAFX/src/controller/AdminDashboardController.java
@@ -32,7 +32,7 @@ import view.ViewManager;
 /**
  * FXML Controller class
  *
- * @author Mahmoud
+ * @author WAFAco
  */
 public class AdminDashboardController implements Initializable {
     public static Users selectedUserToUpdate;
